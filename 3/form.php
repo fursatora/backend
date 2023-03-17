9abdebcaa8844f8a125af6fd5308784e14413e0e
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2023-03-18 01:16:49 +0300

    Update form.php

--- 3/form.php
+++ 3/form.php
@@ -4,9 +4,11 @@
   <meta charset="utf-8">
   <title>Lab 3</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-        crossorigin="anonymous">
+        
+  $(document).ready(function() {
+    $('#example-getting-started').multiselect();
+});
+        
 </head>
 <body>
 <div class="container">

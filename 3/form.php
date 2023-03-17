cc42e78535d276dc2da175871b7f0d7b14008c27
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2023-03-18 00:28:01 +0300

    Update form.php

--- 3/form.php
+++ 3/form.php
@@ -1,12 +1,23 @@
 <form action="" method="POST">
-  <input name="fio" />
-  <select name="year">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
-    }
-    ?>
-  </select>
+   <div class="form-name">
+          <input name="fio" type="text" placeholder="FirstName LastName" id="fio">
+          <label for="fio" class="form-label">Ваше имя</label>
+        </div>
+  <div class="form-email">
+          <input name="email" type="email" placeholder="name@example.com" id="email">
+          <label for="email" class="form-label">Ваш email</label>
+        </div>
+
+<div class="year">
+          <label for="year" class="form-label">Год рождения</label>
+          <select name="year" id="year">
+            <?php 
+                for ($i = 1922; $i <= 2022; $i++) {
+                printf('<option value="%d">%d год</option>', $i, $i);
+                }
+            ?>
+          </select>
+        </div>
   
   <input type="submit" value="ok" />
 </form>

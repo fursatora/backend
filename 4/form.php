3c34cd4b859d36fa5dd9df48ddb68937abd5ccbe
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2023-04-11 00:13:50 +0300

    Update form.php

--- 4/form.php
+++ 4/form.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <title>Lab 4</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-        rel="stylesheet"
+        rel="stylesheet">
   <style>
        .error {
            border: 2px solid red;

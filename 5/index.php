069f9a6fceee4f6bd98ddda949e9d85e7a685917
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2023-05-08 03:27:16 +0300

    Update index.php

--- 5/index.php
+++ 5/index.php
@@ -254,8 +254,8 @@ else{
              $second_stmt -> execute([$app_id, $ability]);
          
         $app_id = $db->lastInsertId();    
-        $third_stmt = $db->prepare("INSERT INTO login SET user_id=?, login=?, pwd=?);
-        $third_stmt->execute(array($app_id, $id, $login, password_hash($pwd, PASSWORD_DEFAULT)));
+        $third_stmt = $db->prepare("INSERT INTO login SET app_id=?,login=?, pwd=?);
+        $third_stmt->execute(array($app_id, $login, password_hash($pwd, PASSWORD_DEFAULT)));
         }
 
     }
